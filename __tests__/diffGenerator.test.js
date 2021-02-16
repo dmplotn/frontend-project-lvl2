662d0167dba1ef136ec8cadb69380e640fd9b413
Dmitry Plotnikov <dmplotn@gmail.com>
2021-02-16 16:49:54 +0500

    add tests

---
+++ __tests__/diffGenerator.test.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import url from 'url';
+import path from 'path';
+import genDiff from '../src/diffGenerator.js';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('plain json', () => {
+  const beforePath = getFixturePath('beforePlain.json');
+  const afterPath = getFixturePath('afterPlain.json');
+  const resultPath = getFixturePath('resultPlain.txt');
+  const result = fs.readFileSync(resultPath, 'utf-8');
+  expect(genDiff(beforePath, afterPath)).toBe(result);
+});
