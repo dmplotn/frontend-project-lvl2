a52b5027504c353dcc81ea9ad2daacce89ad733c
Dmitry Plotnikov <dmplotn@gmail.com>
2021-02-17 22:35:11 +0500

    refactor

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
-import run from '../src/cli.js';
+import { program } from 'commander';
+import packageConfig from '../package.json';
+import generateDiff from '../src/diffGenerator.js';
 
-run();
+program
+  .arguments('<filepath1> <filepath2>')
+  .description(packageConfig.description)
+  .version(packageConfig.version)
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
+
+const [filepath1, filepath2] = program.args;
+
+try {
+  const diff = generateDiff(filepath1, filepath2);
+  console.log(diff);
+} catch (err) {
+  console.log(err.message);
+}
--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -5,26 +5,29 @@ const getDiff = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   const inner = keys
     .flatMap((key) => {
+      let newValue;
       if (!_.has(data1, key)) {
-        return `  + ${key}: ${data2[key]}`;
+        newValue = `  + ${key}: ${data2[key]}`;
+      } else if (!_.has(data2, key)) {
+        newValue = `  - ${key}: ${data1[key]}`;
+      } else if (data1[key] === data2[key]) {
+        newValue = `    ${key}: ${data1[key]}`;
+      } else {
+        newValue = [
+          `  - ${key}: ${data1[key]}`,
+          `  + ${key}: ${data2[key]}`,
+        ];
       }
-      if (!_.has(data2, key)) {
-        return `  - ${key}: ${data1[key]}`;
-      }
-      if (data1[key] === data2[key]) {
-        return `    ${key}: ${data1[key]}`;
-      }
-      return [
-        `  - ${key}: ${data1[key]}`,
-        `  + ${key}: ${data2[key]}`,
-      ];
+      return newValue;
     })
     .join('\n');
+
   return `{\n${inner}\n}`;
 };
 
 export default (filepath1, filepath2) => {
   const data1 = JSON.parse(fs.readFileSync(filepath1, 'utf-8'));
   const data2 = JSON.parse(fs.readFileSync(filepath2, 'utf-8'));
+
   return getDiff(data1, data2);
 };
