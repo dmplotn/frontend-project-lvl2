e44d0d0b2fc0b93fd8807390e3752fdd631871c1
Dmitry Plotnikov <dmplotn@gmail.com>
2021-03-06 12:54:46 +0500

    update tests

--- __tests__/diffGenerator.test.js
+++ __tests__/diffGenerator.test.js
@@ -8,16 +8,21 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const result = fs.readFileSync(getFixturePath('result.txt'), 'utf-8');
+const stylishResult = fs.readFileSync(getFixturePath('stylishResult.txt'), 'utf-8');
+const plainResult = fs.readFileSync(getFixturePath('plainResult.txt'), 'utf-8');
 
 const cases = [
-  ['before.json', 'after.json', result],
-  ['before.yaml', 'after.yaml', result],
+  ['before.json', 'after.json', 'stylish', stylishResult],
+  ['before.yaml', 'after.yaml', 'stylish', stylishResult],
+  ['before.json', 'after.json', 'plain', plainResult],
+  ['before.yaml', 'after.yaml', 'plain', plainResult],
 ];
 
 test.each(cases)(
-  'return diff between %s and %s',
-  (beforeName, afterName, expected) => {
-    expect(genDiff(getFixturePath(beforeName), getFixturePath(afterName))).toBe(expected);
+  'return diff between %s and %s using %s format',
+  (beforeName, afterName, formatName, expected) => {
+    const beforePath = getFixturePath(beforeName);
+    const afterPath = getFixturePath(afterName);
+    expect(genDiff(beforePath, afterPath, formatName)).toBe(expected);
   },
 );
