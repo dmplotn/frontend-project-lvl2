bca6a9c2f4eea2940815833f3180f79688abf007
Dmitry Plotnikov <dmplotn@gmail.com>
2021-03-07 22:33:27 +0500

    refactor

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import packageConfig from '../package.json';
 import generateDiff from '../src/diffGenerator.js';
 
 program
   .arguments('<filepath1> <filepath2>')
-  .description(packageConfig.description)
-  .version(packageConfig.version)
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
   .option('-f, --format [type]', 'output format', 'stylish')
   .parse(process.argv);
 
 const [filepath1, filepath2] = program.args;
 const { format } = program.opts();
 
+let diff;
 try {
-  const diff = generateDiff(filepath1, filepath2, format);
-  console.log(diff);
+  diff = generateDiff(filepath1, filepath2, format);
 } catch (err) {
   console.log(err.message);
 }
+
+console.log(diff);
